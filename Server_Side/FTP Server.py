064a27e5841006c3fb46817d005af0c6765602bf
Landtree <delandtree@gmail.com>
2024-01-20 16:37:29 -0800

    FTP transfers

---
+++ Server_Side/FTP Server.py
@@ -0,0 +1,39 @@
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
+
+def main():
+    # Instantiate a dummy authorizer for managing 'virtual' users
+    authorizer = DummyAuthorizer()
+    #pull this in from an INI created from the GUI
+    #Hardcoded for now
+    pictureStorage = "F:\OneDrive\Pictures\FTP\Scanner"
+    
+    #create the scanner username
+    #probably should change to a different username for production
+    authorizer.add_user('scan','scanner',pictureStorage,perm='elradfmwMT')
+
+    # Instantiate FTP handler class
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    # Define a customized banner (string returned when client connects)
+    handler.banner = "Scanner Connected"
+
+    # Instantiate FTP server class and listen on 0.0.0.0:2121
+    
+    address = ('192.168.50.7', 2121)
+    server = FTPServer(address, handler)
+    
+
+    # set a limit for connections
+    server.max_cons = 5
+    server.max_cons_per_ip = 256
+
+    # start ftp server
+    server.serve_forever()
+
+if __name__ == '__main__':
+    main()
